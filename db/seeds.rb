096e58a686fca3fee198bfc7079e8700d7a3a514
Lex Alexander <l.alexander10@gmail.com>
2015-03-08 18:43:00 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-    user = User.create!(
-        name: "crime",
-        email: "crime@crime.com",
-        age: 22
-
-      )
+    User.create!(
+      email: "jim@jim.com", age: 13, created_at: "2015-03-09 00:02:23", updated_at: "2015-03-09 00:47:08", avatar_file_name: nil, avatar_content_type: nil, avatar_file_size: nil, avatar_updated_at: nil, location: "food", password_digest: "$2a$10$vpiF7Xbd95SAeiqRaP3i1uCT5v4RSY0QiI6H4X9yCQsRes9sSakLe", password: nil)
+    User.create!(
+      email: "jim@jim.com", age: 13, created_at: "2015-03-09 00:02:23", updated_at: "2015-03-09 00:47:08", avatar_file_name: nil, avatar_content_type: nil, avatar_file_size: nil, avatar_updated_at: nil, location: "food", password_digest: "$2a$10$vpiF7Xbd95SAeiqRaP3i1uCT5v4RSY0QiI6H4X9yCQsRes9sSakLe", password: nil)
+    User.create!(
+      email: "jim@jim.com", age: 13, created_at: "2015-03-09 00:02:23", updated_at: "2015-03-09 00:47:08", avatar_file_name: nil, avatar_content_type: nil, avatar_file_size: nil, avatar_updated_at: nil, location: "food", password_digest: "$2a$10$vpiF7Xbd95SAeiqRaP3i1uCT5v4RSY0QiI6H4X9yCQsRes9sSakLe", password: nil)
+    User.create!(
+      email: "jim@jim.com", age: 13, created_at: "2015-03-09 00:02:23", updated_at: "2015-03-09 00:47:08", avatar_file_name: nil, avatar_content_type: nil, avatar_file_size: nil, avatar_updated_at: nil, location: "food", password_digest: "$2a$10$vpiF7Xbd95SAeiqRaP3i1uCT5v4RSY0QiI6H4X9yCQsRes9sSakLe", password: nil)
