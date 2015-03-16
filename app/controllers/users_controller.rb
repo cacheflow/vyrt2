ff1f4e2df9f6dd562453e9050bb153e18979645b
Lex Alexander <l.alexander10@gmail.com>
2015-03-15 23:49:10 -0700

    Remove redirect to sessions page on user create. *Made user create action redirect to users index after save. *Removed links to new sessions path and destroy sessions path within global nav bar.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_session_path
+      redirect_to users_path 
     else 
+      flash[:error] = "You didn't create a new user"
       render :new 
     end
   end
